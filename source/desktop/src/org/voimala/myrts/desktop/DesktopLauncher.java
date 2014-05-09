bbd23829a7e927df741406a1dafdf382309b38ef
Jari Hanhela <jarihanhela@gmail.com>
2014-05-09 12:51:31 +0300

    Init commit

---
+++ source/desktop/src/org/voimala/myrts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.voimala.myrts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.voimala.myrts.app.MyRTS;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyRTS(), config);
+	}
+}
---
+++ source/html/src/org/voimala/myrts/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.voimala.myrts.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.voimala.myrts.app.MyRTS;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyRTS();
+        }
+}
