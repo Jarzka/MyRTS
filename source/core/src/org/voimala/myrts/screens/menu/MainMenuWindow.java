e533d92b2fff6f7e9d5692127ad4ca8580615e4c
Jari Hanhela <jarihanhela@gmail.com>
2014-05-30 23:05:41 +0300

    Main menu buttons added.

--- source/core/src/org/voimala/myrts/screens/menu/MainMenuWindow.java
+++ source/core/src/org/voimala/myrts/screens/menu/MainMenuWindow.java
@@ -4,10 +4,7 @@ package org.voimala.myrts.screens.menu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
 public class MainMenuWindow extends Window {
 
@@ -30,16 +27,52 @@ public class MainMenuWindow extends Window {
         table = new Table();
 
         table.pad(10, 10, 0, 10);
-        table.add(new Label("Audio", skin, "default-font",
-                Color.ORANGE)).colspan(3);
+
+        int buttonsWidth = 200;
+        int buttonsPadding = 10;
+
         table.row();
-        table.columnDefaults(0).padRight(10);
-        table.columnDefaults(1).padRight(10);
+        TextButton textButtonSingleplayer = new TextButton("Singleplayer", skin);
+        textButtonSingleplayer.setWidth(buttonsWidth);
+        textButtonSingleplayer.pad(buttonsPadding);
+        textButtonSingleplayer.setDisabled(true);
+        table.add(textButtonSingleplayer);
+
+        table.row();
+        TextButton textButtonMultiplayer = new TextButton("Multiplayer", skin);
+        textButtonMultiplayer.setWidth(buttonsWidth);
+        textButtonMultiplayer.pad(buttonsPadding);
+        table.add(textButtonMultiplayer);
+
+        table.row();
+        TextButton textButtonSettings = new TextButton("Settings", skin);
+        textButtonSettings.setWidth(buttonsWidth);
+        textButtonSettings.setDisabled(true);
+        textButtonSettings.pad(buttonsPadding);
+        table.add(textButtonSettings);
+
+        table.row();
+        TextButton textButtonCredits = new TextButton("Credits", skin);
+        textButtonCredits.setWidth(buttonsWidth);
+        textButtonCredits.setDisabled(true);
+        textButtonCredits.pad(buttonsPadding);
+        table.add(textButtonCredits);
+
+        table.row();
+        TextButton textButtonQuit = new TextButton("Quit", skin);
+        textButtonQuit.setWidth(buttonsWidth);
+        textButtonQuit.pad(buttonsPadding);
+        textButtonQuit.setDisabled(true);
+        table.add(textButtonQuit);
 
         this.add(table);
     }
 
     private void finalizeWindow() {
+        // Set size
+        setWidth(300);
+        setHeight(500);
+
         // Visual effects
         setColor(1, 1, 1, 0.8f);
 
@@ -48,6 +81,8 @@ public class MainMenuWindow extends Window {
 
         // Let TableLayout recalculate widget sizes and positions
         pack();
+
+        setMovable(false); // TODO Set to true when rendering is fixed.
 
         // Set position
         setPosition(0, 0);
