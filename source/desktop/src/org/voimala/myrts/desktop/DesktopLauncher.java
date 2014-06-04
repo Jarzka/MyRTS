948451f8ab740ed17de3ac8a3e8907e7feb748c8
Jari Hanhela <jarihanhela@gmail.com>
2014-06-04 13:53:27 +0300

    Vsync disabled. The comment included in the desktop launcher tells why.

--- source/desktop/src/org/voimala/myrts/desktop/DesktopLauncher.java
+++ source/desktop/src/org/voimala/myrts/desktop/DesktopLauncher.java
@@ -8,11 +8,17 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "MyRTS";
-        config.width = 800;
-        config.height = 600;
-        config.backgroundFPS = 80;
-        config.foregroundFPS = 80;
-        config.vSyncEnabled = true;
+        config.width = 1024;
+        config.height = 680;
+        /**
+         * FPS in Libgdx means how many times per second the render method will be called.
+         * Since render method also handles game logic and world updating, it should be called
+         * as many times in second as possible. The graphics rendering times per second can be
+         * lowered manually. It seems that vsync needs to be implemented manually.
+         */
+        config.backgroundFPS = 120;
+        config.foregroundFPS = 120;
+        config.vSyncEnabled = false;
 		new LwjglApplication(new GameMain(arg), config);
 	}
 }
