805add3b3275cf132d25ba62fe3745fc22566605
Jari Hanhela <jarihanhela@gmail.com>
2014-07-21 18:51:28 +0300

    M4 moved to myrtsengine package.

---
+++ source/core/src/org/voimala/myrtsengine/screens/gameplay/ammunition/M4Bullet.java
@@ -0,0 +1,6 @@
+package org.voimala.myrtsengine.screens.gameplay.ammunition;
+
+import org.voimala.myrtsengine.screens.gameplay.ammunition.AbstractBullet;
+
+public class M4Bullet extends AbstractBullet {
+}
